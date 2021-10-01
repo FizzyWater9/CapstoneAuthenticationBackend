768119c07a326f116d4b652758f43b1ea20d3095
Skyler Fitzwater <skylerfitzwater9@gmail.com>
2021-09-30 20:33:01 -0600

    Adding MySQL, Adding adduser request, adding getuser request

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ const cors = require("cors")({
     origin: true
 });
 
-/*
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     // properties....
     host: 'localhost',
-    user: 'sqlservice',
-    password: 'LoveTheService',
-    database: 'Users'
+    user: 'root',
+    password: 'root',
+    database: 'users'
 });
 
 connection.connect( (error) => {
     if(!!error) {
         console.log('Error');
+        console.log(error);
     } else {
         console.log('Connected');
     }
 });
-*/
+
 
 app.listen(8800, () => {
     console.log(`App listening on port 8800`);
@@ -47,7 +47,37 @@ app.get("/adduser", (req, res) => {
         if (req.method === "OPTIONS") {
             res.status(200).send();
         } else {
-            res.status(200).send("This is how you would add a user.");
+            
+            let firstName = req.query.firstname;
+            let lastName = req.query.lastname;
+
+            const sqlInsert = `INSERT INTO users (firstname, lastname) VALUES (?,?)`;
+            const valuesToAdd = [firstName, lastName];
+
+            connection.query(sqlInsert, valuesToAdd, (err) => {
+                if(err) {
+                    throw err;
+                } else {
+                    res.status(200).send("User was added.");
+                }
+            })
+            
         }
     });
 });
+
+app.get("/getuser", (req, res) => {
+    cors(req, res, () => {
+        if (req.method === "OPTIONS") {
+            res.status(200).send();
+        } else {
+            connection.query("SELECT * FROM users", (err, result) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.status(200).send(result);
+                }
+            })
+        }
+    })
+})
